2e9e220bd770652c315aca59d2af0352671970f3
aaron.otaku@gmail.com <aaron.otaku@gmail.com@206d28dd-53d7-8602-f6f5-db4968d65ac7>
2012-04-12 16:23:05 +0000

    Fixed the dropdown register box
    git-svn-id: http://roc-cf.googlecode.com/svn/trunk@7 206d28dd-53d7-8602-f6f5-db4968d65ac7

--- themes/Default/js/frontpage.js
+++ themes/Default/js/frontpage.js
@@ -1,26 +1,30 @@
 $(document).ready(function(){
 	$("span.joinnow").click(function(){
-		$('.barline').animate({
-			height: '264px'
-		}, 'slow');
-		$("span.joinnow").addClass("active");
-		$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
-		$("span.joinnow").hover(function(){
-			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip_over.png) no-repeat');
-		},function(){
-			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
-		});
-	});
-	$("span.join .active").click(function(){
-		$('.barline').animate({
-			height: '40px'
-		}, 'slow');
-		$("span.joinnow").removeClass("active");
-		$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow.png) no-repeat');
-		$("span.joinnow").hover(function(){
-			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_over.png) no-repeat');
-		},function(){
+		if ($("span.joinnow").hasClass("active"))
+		{
+			$('.barline').animate({
+				height: '40px'
+			}, 'slow');
+			$("span.joinnow").removeClass("active");
 			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow.png) no-repeat');
-		});
+			$("span.joinnow").hover(function(){
+				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_over.png) no-repeat');
+			},function(){
+				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow.png) no-repeat');
+			});
+		}
+		else
+		{
+			$('.barline').animate({
+				height: '264px'
+			}, 'slow');
+			$("span.joinnow").addClass("active");
+			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
+			$("span.joinnow").hover(function(){
+				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip_over.png) no-repeat');
+			},function(){
+				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
+			});
+		}
 	});
 });
