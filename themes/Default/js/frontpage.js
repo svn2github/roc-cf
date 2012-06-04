31046884c84fb3362e582686ae57dbf3af6a5ff2
me@n0minal.co.uk <me@n0minal.co.uk@206d28dd-53d7-8602-f6f5-db4968d65ac7>
2012-06-04 20:51:05 +0000

    git-svn-id: http://roc-cf.googlecode.com/svn/trunk@60 206d28dd-53d7-8602-f6f5-db4968d65ac7

--- themes/Default/js/frontpage.js
+++ themes/Default/js/frontpage.js
@@ -100,11 +100,11 @@ $(document).ready(function(){
 			}, 'slow');
 			$('span.joinnow').text('Join now');
 			$("span.joinnow").removeClass("active");
-			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow.png) no-repeat');
+			$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow.png) no-repeat');
 			$("span.joinnow").hover(function(){
-				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_over.png) no-repeat');
+				$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow_over.png) no-repeat');
 			},function(){
-				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow.png) no-repeat');
+				$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow.png) no-repeat');
 			});
 			$('.header').animate({
 				height: '250px',
@@ -118,11 +118,11 @@ $(document).ready(function(){
 			}, 'slow');
 			$('span.joinnow').text('Sign-in');
 			$("span.joinnow").addClass("active");
-			$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
+			$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow_flip.png) no-repeat');
 			$("span.joinnow").hover(function(){
-				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip_over.png) no-repeat');
+				$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow_flip_over.png) no-repeat');
 			},function(){
-				$("span.joinnow").css('background', 'url(img/frontpage/joinnow_arrow_flip.png) no-repeat');
+				$("span.joinnow").css('background', 'url(themes/Default/img/frontpage/joinnow_arrow_flip.png) no-repeat');
 			});
 			$('.header').animate({
 				height: '0px',
